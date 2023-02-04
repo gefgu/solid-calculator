a26da83b00d5f7113dada347c34cf86d5d1ae72c
Gustavo Santos <gefgu@hotmail.com>
2023-02-04 15:28:56 -0300

    feat: add initial calculator top

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 export default function CalculatorPage() {
   return (
     <main class="h-screen w-full flex flex-col items-center justify-center bg-gradient-to-r from-[#807ECE] to-[#8E7ECE] text-white">
-      <div class="w-full max-w-sm h-96 rounded-3xl mx-auto shadow-2xl bg-[#2D2A37] p-8">
-        <div class="grid gap-8">
-          <div>1 + 1</div>
-          <div class="flex justify-between">
-            <div>=</div>
-            <div>2</div>
+      <div class="w-full max-w-sm h-96 rounded-3xl mx-auto shadow-2xl bg-[#2D2A37] p-12">
+        <div class="grid gap-4">
+          <div class="flex justify-end text-[#6B6B6B] text-lg">1 + 1</div>
+          <div class="flex justify-between items-center">
+            <div class="text-[#6B6B6B] text-xl">=</div>
+            <div class="text-3xl">2</div>
           </div>
         </div>
         <div>{/* bottom */}</div>

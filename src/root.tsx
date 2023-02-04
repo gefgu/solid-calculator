06d2923b638a702653a4444fcaf18e843d31b515
Gustavo Santos <gefgu@hotmail.com>
2023-02-04 15:26:03 -0300

    feat: add rubik font

--- src/root.tsx
+++ src/root.tsx
@@ -6,6 +6,7 @@ import {
   FileRoutes,
   Head,
   Html,
+  Link,
   Meta,
   Routes,
   Scripts,
@@ -20,6 +21,12 @@ export default function Root() {
         <Title>Calculator</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Link rel="preconnect" href="https://fonts.googleapis.com" />
+        <Link rel="preconnect" href="https://fonts.gstatic.com" />
+        <Link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Body>
         <Suspense>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,16 @@
 export default function CalculatorPage() {
   return (
-    <main class="h-screen w-full flex flex-col items-center justify-center bg-gradient-to-r from-[#807ECE] to-[#8E7ECE]">
-      <div class="w-full max-w-sm h-96 rounded-3xl mx-auto shadow-2xl bg-[#2D2A37] p-8"></div>
+    <main class="h-screen w-full flex flex-col items-center justify-center bg-gradient-to-r from-[#807ECE] to-[#8E7ECE] text-white">
+      <div class="w-full max-w-sm h-96 rounded-3xl mx-auto shadow-2xl bg-[#2D2A37] p-8">
+        <div class="grid gap-8">
+          <div>1 + 1</div>
+          <div class="flex justify-between">
+            <div>=</div>
+            <div>2</div>
+          </div>
+        </div>
+        <div>{/* bottom */}</div>
+      </div>
     </main>
   );
 }

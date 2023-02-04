035128c1e0ea77c776941fdc982ee6a458a5a827
Gustavo Santos <gefgu@hotmail.com>
2023-02-04 15:21:50 -0300

    feat: add calculator shape

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
 export default function CalculatorPage() {
   return (
-    <main class="h-screen w-full bg-gradient-to-r from-[#807ECE] to-[#8E7ECE]"></main>
+    <main class="h-screen w-full flex flex-col items-center justify-center bg-gradient-to-r from-[#807ECE] to-[#8E7ECE]">
+      <div class="w-full max-w-sm h-96 rounded-3xl mx-auto shadow-2xl bg-[#2D2A37] p-8"></div>
+    </main>
   );
 }
